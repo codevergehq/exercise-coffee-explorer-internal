4676579191aeeff0720558cabcba7e6f17844543
Alexander Schnebel <alexander.schnebel@codeverge.de>
2025-02-11 09:30:11 +0100

    add mobile friendly version

--- src/routes/shared/CoffeeLayout.tsx
+++ src/routes/shared/CoffeeLayout.tsx
@@ -1,19 +1,33 @@
 import { Outlet, useSearchParams } from "react-router";
 import CoffeeList from "./components/CoffeeList";
 import SearchAndFilter from "./components/SearchAndFilter";
+import { useEffect, useState } from "react";
 
 function CoffeeLayout() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
     const [searchParams] = useSearchParams()
-    const showList = searchParams.get("showSearchBar") === "true"
+    const showSearchBarParam = searchParams.get("showSearchBar")
+    const showList = showSearchBarParam === null || showSearchBarParam === "true"
+
+    useEffect(() => {
+        setIsSidebarOpen(showList)
+    }, [showList])
 
     return (
-        <div className="flex flex-col md:flex-row h-screen bg-zinc-50">
-            {showList && (
-                <div className="flex flex-col gap-4 w-full md:max-w-xs p-4 bg-white overflow-auto">
-                    <SearchAndFilter />
-                    <CoffeeList />
-                </div>
-            )}
+        <div className="relative flex flex-col md:flex-row h-screen bg-zinc-50">
+            <div
+                className={`
+                    flex flex-col gap-4 
+                    w-full md:max-w-xs p-4
+                    bg-white overflow-auto
+                    absolute md:relative z-10 h-full
+                    transition-transform duration-300
+                    ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}
+                `}
+            >
+                <SearchAndFilter />
+                <CoffeeList />
+            </div>
             <div className="w-full md:max-w-3xl p-4">
                 <Outlet />
             </div>
--- src/routes/shared/Layout.tsx
+++ src/routes/shared/Layout.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import { useSearchParams } from "react-router";
 
 function Layout({ children }: { children: React.ReactNode }) {
+    const [ isSidebarOpen, setIsSidebarOpen ] = useState(true)
     const [searchParams, setSearchParams] = useSearchParams()
-    const showSearchBar = searchParams.get("showSearchBar") === "true"
+    const showSearchBarParam = searchParams.get("showSearchBar")
+    const showSearchBar = showSearchBarParam === null || showSearchBarParam === "true"
 
     const onToggleSearchBar = (show: boolean) => {
+        setIsSidebarOpen(show)
         setSearchParams({ 
             ...Object.fromEntries(searchParams.entries()),
             showSearchBar: show ? "true" : "false"
@@ -18,7 +22,7 @@ function Layout({ children }: { children: React.ReactNode }) {
             <header className="flex bg-white shadow-md px-4 py-2">
                 <Button variant="ghost" size="icon" onClick={() => onToggleSearchBar(!showSearchBar)}>
                     <span className="sr-only">Toggle Search Bar</span>
-                    {showSearchBar ? <X className="size-4" /> : <Menu className="size-4" />}
+                    {isSidebarOpen ? <X className="size-4" /> : <Menu className="size-4" />}
                 </Button>
             </header>
             <main className="md:flex-row h-screen bg-zinc-50 relative">
